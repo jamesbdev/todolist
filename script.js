75b51d00f2fdd99b4473939b26a656ae11317152
jaybaybremner <j.bremner@ymail.com>
2020-12-27 17:58:16 +0000

    cleaning up script

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 
-//when user submits new task
-// take the value of the input field
-// add this to a li element 
-// append the li element to the list 
-//when user clicks on submit, print value to the console
-
 const submitBtn = document.getElementById('submit');
 
 //adds a new list item to the to do list containg the value of the input
 function addListItem(){
     const listItem = document.createElement('li');
+    listItem.classList.add("list-item");
     const task = document.createTextNode($('#task').val());
     listItem.appendChild(task);
     const todoList = document.getElementById('todolist');
@@ -18,19 +13,18 @@ function addListItem(){
     $('#task').val(''); 
 }
 
-//write a function to remove a list item when it is clicked
+const listItem = document.getElementsByClassName('list-item');
+
+//loop through li elements
+for (let item of listItem) {
+    item.addEventListener('click', function(){
+       item.style.display = 'none';
+       console.log('clicked');
+    });
+}
+
 
 //run function to add list item when user clicks on button
 submitBtn.addEventListener('click', addListItem);
   
  
-
-
-
-//create li element 
-//add value of input field 
-//append new li element to list
-
-
-// when a user clicks on an element in the list
-// remove the element from the list 
