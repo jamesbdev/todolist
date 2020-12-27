2e3a7ae42f65568429c1929924c42d2a62f7521d
jaybaybremner <j.bremner@ymail.com>
2020-12-27 12:22:42 +0000

    adding Bootstrap

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-console.log('hello world');
 
+
+//when user submits new task
+// take the value of the input field
+// add this to a li element 
+// append the li element to the list 
+
+
+//when user clicks on submit, print value to the console
+const submitBtn = document.getElementById('submit');
+submitBtn.addEventListener('click', function(){
+    console.log('test');
+})
+
+// when a user clicks on an element in the list
+// remove the element from the list 
